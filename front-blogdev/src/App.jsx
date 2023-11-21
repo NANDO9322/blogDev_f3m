6d9cd96107491cd79e988c778b3def12c96bb1a6
alee <alee_meier@outlook.com>
2023-11-21 12:33:08 -0300

    Aula 2111 - Criando loading e contexto

--- front-blogdev/src/App.jsx
+++ front-blogdev/src/App.jsx
@@ -1,29 +1,76 @@
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate, Form } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { userAuthentication } from "./hooks/userAuthentication";
+
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = userAuthentication();
+
+  const loadingUser = user === undefined;
+
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth])
+
+  if (loadingUser) {
+    return (
+      <div className="container load">
+        <div
+          aria-label="Orange and tan hamster running in a metal wheel"
+          role="img"
+          class="wheel-and-hamster"
+        >
+          <div class="wheel"></div>
+          <div class="hamster">
+            <div class="hamster__body">
+              <div class="hamster__head">
+                <div class="hamster__ear"></div>
+                <div class="hamster__eye"></div>
+                <div class="hamster__nose"></div>
+              </div>
+              <div class="hamster__limb hamster__limb--fr"></div>
+              <div class="hamster__limb hamster__limb--fl"></div>
+              <div class="hamster__limb hamster__limb--br"></div>
+              <div class="hamster__limb hamster__limb--bl"></div>
+              <div class="hamster__tail"></div>
+            </div>
+          </div>
+          <div class="spoke"></div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />}></Route>
-            <Route path='/about' element={<About />}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
